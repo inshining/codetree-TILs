de9ff9ada0032d73dd76323f4a2bd775426bf8b2
inshining <86528640+inshining@users.noreply.github.com>
2024-04-09 18:52:37 +0900

    Title: 코드트리 메신저, Time: 1508ms, Memory: 61MB, Status: Passed - CodeTree

--- 240409/코드트리 메신저/codetree-messenger.py
+++ 240409/코드트리 메신저/codetree-messenger.py
@@ -1,36 +1,111 @@
 N, Q = map(int, input().split())
 first_line = list(map(int, input().split()))
-parents = [-1] + first_line[1:N+1][:]
+parents = [0] + first_line[1:N+1][:]
 powers = [0] + first_line[N+1:][:]
 alarms = [True] * (N+1)
+val = [0] * (N+1)
 
-def go_up(_from, target, p):
-    if not alarms[_from]:
-        return
-    if p <= 0:
-        return
-    if parents[_from] == target:
-        global ans
-        ans += 1
-        return
-    go_up(parents[_from], target, p-1)
+MAX_D = 22
+dp = [[0] * MAX_D for _ in range(N+1)]
 
+def init():
+    for i in range(1, N+1):
+        if powers[i] > 20:
+            powers[i] = 20
 
-for turn in range(Q-1):
-    line = list(map(int, input().split()))
-    command = line[0]
+    for idx in range(1, N+1):
+        cur = idx
+        x = powers[idx]
+        dp[cur][x] += 1
+        while parents[cur] and x:
+            cur = parents[cur]
+            x -= 1
+            if x:
+                dp[cur][x] += 1
+            val[cur] += 1
+def toggle_alarm(c):
+    toggle = alarms[c]
+    p = parents[c]
+    num = 1
+    while p:
+        for i in range(num, MAX_D):
+            val[p] += (-dp[c][i]) if toggle else dp[c][i]
+            if i - num > 0:
+                dp[p][i-num] += (-dp[c][i]) if toggle else dp[c][i]
+        if not alarms[p]:
+            break
+        num += 1
+        p = parents[p]
+    alarms[c] = not alarms[c]
+
+def change_power(c, power):
+    before_p = powers[c]
+    power = min(power, 20)
+    powers[c] = power
+
+    dp[c][before_p] -= 1
+
+    if alarms[c]:
+        cur = parents[c]
+        num = 1
+        while cur:
+            if before_p >= num:
+                val[cur] -= 1
+            if before_p > num:
+                dp[cur][before_p-num] -= 1
+            if not alarms[cur]:
+                break
+            num += 1
+            cur = parents[cur]
+
+    dp[c][power] += 1
+
+    if alarms[c]:
+        cur = parents[c]
+        num = 1
+        while cur:
+            if power >= num:
+                val[cur] += 1
+            if power > num:
+                dp[cur][power-num] += 1
+            if not alarms[cur]:
+                break
+            num += 1
+            cur = parents[cur]
+
+def change_chatter(chat1, chat2):
+    ac1 = alarms[chat1]
+    ac2 = alarms[chat2]
+
+    if ac1:
+        toggle_alarm(chat1)
+    if ac2:
+        toggle_alarm(chat2)
+
+    parents[chat1], parents[chat2] = parents[chat2], parents[chat1]
+
+    if ac1:
+        toggle_alarm(chat1)
+    if ac2:
+        toggle_alarm(chat2)
+
+def retrieve_chat(c):
+    print(val[c])
+
+init()
+
+for _ in range(Q-1):
+    q = list(map(int, input().split()))
+    command = q[0]
     if command == 200:
-        _, c = line
-        alarms[c] = not alarms[c]
+        c = q[1]
+        toggle_alarm(c)
     elif command == 300:
-        _, c, power = line
-        powers[c] = power
+        _, c, power = q
+        change_power(c, power)
     elif command == 400:
-        _, c1, c2 = line
-        parents[c1], parents[c2] = parents[c2], parents[c1]
+        _, c1, c2 = q
+        change_chatter(c1, c2)
     elif command == 500:
-        c = line[1]
-        ans = 0
-        for i in range(N, c, -1):
-            go_up(i, c, powers[i])
-        print(ans)
+        c = q[-1]
+        retrieve_chat(c)
