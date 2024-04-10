0ccc802ed1f90e326a5fce0065feceb1a652a404
inshining <86528640+inshining@users.noreply.github.com>
2024-04-10 13:30:22 +0900

    Title: 불안한 무빙워크, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/불안한 무빙워크/unstable-moving-walk.py
+++ 240410/불안한 무빙워크/unstable-moving-walk.py
@@ -1,42 +1,54 @@
 N, K = map(int, input().split())
 moving_walk = list(map(int, input().split()))
-persons = [1] + [0] * (N-1)
+persons = [0] * (N*2)
+start_index = 0
 
 def move():
-    back = moving_walk.pop(-1)
-    moving_walk.insert(0, back)
-
-    next_people = [0] * N
-    for i in range(N-2, -1, -1):
-        next_people[i+1] = persons[i]
-    for i in range(N):
-        persons[i] = next_people[i]
-
+    # back = moving_walk.pop(-1)
+    # moving_walk.insert(0, back)
+    #
+    # next_people = [0] * N
+    # for i in range(N-2, -1, -1):
+    #     next_people[i+1] = persons[i]
+    # for i in range(N):
+    #     persons[i] = next_people[i]
+    global start_index
+    start_index = (start_index -1) % (N * 2)
 def walk():
-    next_people = [0] * N
-    for i in range(N-2, -1, -1):
-        if persons[i]:
-            if not next_people[i+1] and moving_walk[i+1] > 0:
-                next_people[i+1] = 1
-                moving_walk[i+1] -= 1
-    for i in range(N):
-        persons[i] = next_people[i]
+    global start_index
+    next_people = [0] * (N * 2)
+    j = 0
+    while j < N:
+        idx = (start_index + j) % (N*2)
+        if persons[idx]:
+            next_idx = (idx+1) % (N*2)
+            if not next_people[next_idx] and moving_walk[next_idx] > 0:
+                next_people[next_idx] = 1
+                moving_walk[next_idx] -= 1
+        j += 1
+
+    for j in range(N):
+        idx = (start_index + j) % (N*2)
+        persons[idx] = next_people[idx]
 
 def go_up():
-    if not persons[0] and moving_walk[0] > 0:
-        moving_walk[0] -= 1
-        persons[0] = 1
+    global start_index
+    if not persons[start_index] and moving_walk[start_index] > 0:
+        moving_walk[start_index] -= 1
+        persons[start_index] = 1
 def count_block():
     num = 0
-    for i in range(N):
+    for i in range(2 * N):
         if moving_walk[i] <= 0:
            num += 1
     if num >= K:
         return True
     return False
 def go_down():
-    if persons[N-1]:
-        persons[N-1] = 0
+    global start_index
+    end_idx = (start_index + N - 1) % (2 * N)
+    if persons[end_idx]:
+        persons[end_idx] = 0
 i = 1
 while True:
     move()
