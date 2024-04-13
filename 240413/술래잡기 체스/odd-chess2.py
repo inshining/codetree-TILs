d30b5f701749b2c685545416e3bda79ae1d0dec8
inshining <86528640+inshining@users.noreply.github.com>
2024-04-13 16:40:38 +0900

    Title: 술래잡기 체스, Time: 157ms, Memory: 28MB, Status: Passed - CodeTree

--- 240413/술래잡기 체스/odd-chess2.py
+++ 240413/술래잡기 체스/odd-chess2.py
@@ -1,7 +1,9 @@
 N = 4
 H = 16
+TAGGER = (-2, -2)
+BLANK = (-1,-1)
 dead = [False] * (H+1)
-directions = [0] * (H+1)
+hs = [(0,0,0)] + [0] * (H)
 board = [[0] * N for _ in range(N)]
 
 dy = [0, -1, -1, 0, 1, 1, 1, 0, -1]
@@ -10,71 +12,97 @@ for y in range(N):
 	line = list(map(int, input().split()))
 	for i in range(0,8,2):
 		p, d = line[i], line[i+1]
-		board[y][i//2] = p
-		directions[p] = d
+		x=i // 2
+		board[y][x] = (p,d)
 
-def kill(y,x):
-	p = board[y][x]
-	dead[p] = True
-	board[y][x] = 0
-	return directions[p]
 def out(y,x):
 	return y <0 or N <= y or x < 0 or N <= x
 
-def other_move():
-	hoses = [[-1,-1]] * (H+1)
+def move(idx):
 	for y in range(N):
 		for x in range(N):
-			if board[y][x] > 0:
-				hoses[board[y][x]] = [y,x]
+			pn, pd = board[y][x]
+			if pn == idx:
+				for j in range(8):
+					nd = (pd + j) % len(dx)
+					if nd == 0:
+						nd = 1
+					ny = y + dy[nd]
+					nx = x + dx[nd]
+					if out(ny, nx):
+						continue
+					if board[ny][nx] == TAGGER:
+						continue
+
+					board[y][x] = (pn, nd)
+					board[y][x], board[ny][nx] = board[ny][nx], board[y][x]
+					return
+
+
+def move_all():
 	for i in range(1, H+1):
-		if dead[i]:
+		move(i)
+
+def choose_one(ty,tx, td, hs,dead):
+	board = [[0] * N for _ in range(N)]
+	for i in range(1, H+1):
+		if i in dead:
 			continue
-		y,x = hoses[i]
-		d = directions[i]
-		for j in range(8):
-			nd = (d+j) % len(dx)
-			ny = y + dy[nd]
-			nx = x + dx[nd]
-			if out(ny,nx):
-				continue
-			if (ny,nx) == (Ty, Tx):
-				continue
+		r,c,_ = hs[i]
+		board[r][c] = i
 
-			counter = board[ny][nx]
-			board[ny][nx] = i
-			board[y][x] = counter
-			hoses[counter] = [y,x]
-			directions[i] = nd
-			break
-
-def choose_one():
 	can = []
 	i = 1
 	while True:
-		ny = Ty + dy[Td] * i
-		nx = Tx + dx[Td] * i
+		ny = ty + dy[td] * i
+		nx = tx + dx[td] * i
 		if out(ny,nx):
 			break
 		if board[ny][nx] > 0:
-			can.append((board[ny][nx], ny,nx))
+			can.append((ny,nx))
 		i += 1
 	return can
 
-Ty, Tx = 0, 0
-Td = kill(Ty, Tx)
-while True:
-	other_move()
-	can = choose_one()
-	if can:
-		can.sort(key=lambda x : -x[0])
-		_, y,x = can[0]
-		Ty, Tx = y,x
-		Td = kill(y,x)
-	else:
-		break
-ans = 0
-for i in range(1, H+1):
-	if dead[i]:
-		ans += i
+def is_tagger_going(y,x):
+	return not out(y, x) and board[y][x] != BLANK
+
+
+def done(y,x,d):
+	for i in range(1, N+1):
+		ny,nx = y + dy[d] * i, x + dx[d] * i
+		if is_tagger_going(ny,nx):
+			return False
+	return True
+def go(y,x,d, score):
+	global ans
+	if done(y,x,d):
+		ans = max(ans, score)
+		return
+	for dist in range(1, N+1):
+		ny, nx = y + dy[d] * dist, x + dx[d] * dist
+		if not is_tagger_going(ny,nx):
+			continue
+
+		temp = [
+			[board[i][j] for j in range(N)]
+			for i in range(N)
+		]
+
+		extra_score, next_dir = board[ny][nx]
+		board[ny][nx], board[y][x] = TAGGER, BLANK
+
+		move_all()
+
+		go(ny,nx, next_dir, score+extra_score)
+
+		for i in range(N):
+			for j in range(N):
+				board[i][j] = temp[i][j]
+
+ans = -1
+score, td = board[0][0]
+board[0][0] = TAGGER
+move_all()
+
+go(0,0,td,score)
 print(ans)
