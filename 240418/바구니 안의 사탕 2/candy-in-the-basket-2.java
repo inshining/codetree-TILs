fc0707ee4117d6690e946b0a2f4eaf210a7aa228
inshining <86528640+inshining@users.noreply.github.com>
2024-04-18 15:16:33 +0900

    Title: 바구니 안의 사탕 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240418/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static int N, K;
+    public static int[] arr= new int[101];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        int num, pos;
+
+        for (int i = 0; i < N; i++){
+            num = sc.nextInt();
+            pos = sc.nextInt();
+            arr[pos] = num;
+        }
+
+        int ans = 0;
+        for (int c = K; c < 100 - K + 1; c++){
+            num = 0;
+            for (int j = -K; j < K+1; j++){
+                num += arr[c+j];
+            }
+            ans = Math.max(ans, num);
+        }
+        System.out.println(ans);
+
+
+    }
+}
