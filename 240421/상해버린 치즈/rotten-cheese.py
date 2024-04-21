9d01893f9a9cefc6541a62b4c583cff02cf91e21
inshining <86528640+inshining@users.noreply.github.com>
2024-04-21 18:25:59 +0900

    Title: 상해버린 치즈, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240421/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,23 @@
+N, M, D, S = map(int, input().split())
+records = [dict() for _ in range(M+1)]
+sicks = dict()
+for i in range(D):
+    p,m,t = map(int, input().split())
+    records[m][p] = t
+
+for i in range(S):
+    p, t = map(int, input().split())
+    sicks[p] = t
+
+ans = 0
+for i in range(1, M+1):
+    persons = records[i]
+    isCan = True
+    for k in persons.keys():
+        if k in sicks and sicks[k] <= persons[k]:
+            isCan = False
+            break
+    if isCan:
+        ans = max(ans, len(persons))
+
+print(ans)
