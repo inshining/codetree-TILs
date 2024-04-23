21e8240e8ac71ccd47ac49c4615ae22b8dcbed02
inshining <86528640+inshining@users.noreply.github.com>
2024-04-23 17:03:38 +0900

    Title: 흥미로운 숫자 2, Time: 164ms, Memory: 19MB, Status: Passed - CodeTree

--- 240423/흥미로운 숫자 2/interesting-numbers-2.java
+++ 240423/흥미로운 숫자 2/interesting-numbers-2.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Main {
     public static int X, Y;
-    public static Map<Integer, Integer> counter;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         X = sc.nextInt();
@@ -9,20 +8,20 @@ public class Main {
 
         int ans = 0;
         for (int i = X; i <= Y; i++){
-            counter = new HashMap<Integer, Integer>();
+            int[] counter = new int[10];
             int num = i;
-            for (int j = 0; j< 7; j++){
-                int idx = num % 10;
+            int digits = 0;
+            while (num> 0){
+                counter[num % 10]++;
                 num = num / 10;
-                if (counter.containsKey(idx)){
-                    counter.put(idx, counter.get(idx)+1);
-                } else{
-                    counter.put(idx, 1);
-                }
-                if (counter.size() > 2) break;
+                digits++;
             }
-            for (int v : counter.values()){
-                if (v == (int)(Math.log10(i))) ans++;
+
+            for (int j = 0; j <10; j++){
+                if (counter[j] == digits -1){
+                    ans++;
+                    break;
+                }
             }
         }
 
