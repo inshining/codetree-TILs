5f50348d67f41b8efa0293ea8c1fde34ca6bc99e
inshining <86528640+inshining@users.noreply.github.com>
2024-05-01 14:04:10 +0900

    Title: 독서실의 거리두기 5, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240501/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 240501/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -6,46 +6,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         String line = sc.next();
+        int p = 0;
         for (int i = 0; i < N; i++){
             char k =  line.charAt(i);
             if (k == '1'){
-                arr[i] = 1;
-            } else{
-                arr[i]= 0;
-            }
+                arr[p] = i;
+                p++;
+            } 
         }
 
-        int ans = Integer.MIN_VALUE;
-        for (int i =0; i< N; i++){
-            if (arr[i] == 1){
-                continue;
-            }
-            int left = i;
-            int dis = Integer.MAX_VALUE;
-            while (left >= 0){
-                if (arr[left]==1){
-                    break;
-                }
-                left--;
-            }
-            if (left >= 0){
-                dis = i - left;
-            }
+        int ans = Integer.MAX_VALUE;
 
-            int right = i;
-            while (right < N){
-                if (arr[right]== 1){
-                    break;
+        if (p==1){
+            ans = Math.max(N-arr[0], arr[0]);
+        } else{
+            for (int i = 1; i < p; i++){
+                ans = Math.min(ans, arr[i] - arr[i-1]);
+            }
+            int total = Integer.MIN_VALUE;
+            for (int i =0; i< N; i++){
+                if (line.charAt(i) == '0'){
+                    int dis = Integer.MAX_VALUE;
+                    for (int j = 0; j < p; j++){
+                        dis = Math.min(dis, Math.abs(arr[j] - i));
+                    }
+                    // System.out.println(dis);
+                    total = Math.max(total, dis);
                 }
-                right++;
             }
-            if (right < N){
-                dis = Math.min(dis, right-i);
-            }
-            ans = Math.max(ans, dis);
-
-            // System.out.println(ans + " " + i + " " + left + " " + right);
+            ans = Math.min(total, ans);
         }
+
         System.out.println(ans);
     }
 }
