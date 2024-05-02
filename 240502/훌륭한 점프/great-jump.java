d5002f2216063ab427375e7a0b64ea515e6f7752
inshining <86528640+inshining@users.noreply.github.com>
2024-05-02 16:58:48 +0900

    Title: 훌륭한 점프, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240502/훌륭한 점프/great-jump.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Main {
+    public static int N, K;
+    public static int[] arr = new int[101];
+    public static boolean isPossible(int val){
+        int[] temp = new int[N];
+        int cnt = 0;
+        for (int i = 0; i < N; i++){
+            if (arr[i] <= val){
+                temp[cnt++] = i;
+            }
+        }
+        for (int i = 1; i < N; i++){
+            if (temp[i] - temp[i-1] > K){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        for (int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+        int ans = 101;
+        for (int i = 100; i >= Math.max(arr[0], arr[N]);i--){
+            if (isPossible(i)){
+                ans = Math.min(ans, i);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
