3ed3b2f8d07d7a1eb1dec38f15e5940445202ee5
inshining <86528640+inshining@users.noreply.github.com>
2024-05-04 20:27:37 +0900

    Title: 언덕 깎기, Time: 159ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240504/언덕 깎기/hill-cutting.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+    public static int N;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N= sc.nextInt();
+        arr = new int[N];
+        for (int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int ans = Integer.MAX_VALUE;
+        for(int i = 4; i <= 4; i++){
+            int total = 0;
+            for (int j = 0; j < N; j++){
+                int k;
+                if (arr[j] < i ){
+                    k = Math.abs(arr[j] - i);
+                    total += k * k;
+                } else if(i + 17 < arr[j]){
+                    k = Math.abs(arr[j] - (i+17));
+                    total += k * k;
+                }
+            }
+            ans = Math.min(ans, total);
+        }
+        System.out.println(ans);
+
+    }
+}
