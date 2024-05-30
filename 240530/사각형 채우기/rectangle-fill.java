fcfae587554e1307252e1a92ee0f8105d5fb0e98
inshining <86528640+inshining@users.noreply.github.com>
2024-05-30 21:06:46 +0900

    Title: 사각형 채우기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 240530/사각형 채우기/rectangle-fill.java
+++ 240530/사각형 채우기/rectangle-fill.java
@@ -9,7 +9,7 @@ public class Main {
         arr[1] = 1;
         arr[2] = 2;
         for (int i = 3; i <= N; i++){
-            arr[i] = arr[i-1] + arr[i-2];
+            arr[i] = (arr[i-1] + arr[i-2]) % 10007;
         }
         System.out.println(arr[N]);
     }
