55ba3efd5e5964eb6c897d72d2c7a9f415e52d20
inshining <86528640+inshining@users.noreply.github.com>
2024-06-04 17:11:50 +0900

    Title: 밭의 높이를 고르게하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240604/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+    public static int N, H, T;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        H = sc.nextInt();
+        T = sc.nextInt();
+
+        arr = new int[N];
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i <N; i++){
+            arr[i] = Math.abs(sc.nextInt() - H);
+        }
+
+        for (int i = 0; i < N;i++){
+            int num = 0;
+            for (int j = 0; j < T; j++){
+                num += arr[i+j];
+            }
+            ans = Math.min(ans, num);
+        }
+        
+        System.out.println(ans);
+    }
+}
