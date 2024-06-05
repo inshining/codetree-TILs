8c168b2341bbada03a84f764b071c31cf15dbb2a
inshining <86528640+inshining@users.noreply.github.com>
2024-06-05 17:39:26 +0900

    Title: 수열의 순서 바꾸기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240605/수열의 순서 바꾸기/reorder-sequence.py
+++ 240605/수열의 순서 바꾸기/reorder-sequence.py
@@ -1,26 +1,9 @@
 N = int(input())
 vs = list(map(int, input().split()))
 
-def is_sort():
-    for i in range(N-1):
-        if vs[i] > vs[i+1]:
-            return False
-    return True
-
-ans = 0
-while not is_sort():
-    v = vs.pop(0)
-    
-    e = 0
-    for i in range(N-2, 0, -1):
-        if vs[i-1] > vs[i]:
-            e = i
-    
-    ins = e
-    while ins < N-1:
-        if v < vs[ins]:
-            break
-        ins += 1
-    vs.insert(ins, v)
-    ans += 1
-print(ans)
+end = 0
+for i in range(N-1, 0, -1):
+    if vs[i-1] > vs[i]:
+        end = i
+        break
+print(end)
