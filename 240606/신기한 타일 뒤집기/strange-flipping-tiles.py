dd84689a2b9e35c4d213923e80e9e45ea4865dbf
inshining <86528640+inshining@users.noreply.github.com>
2024-06-06 17:43:47 +0900

    Title: 신기한 타일 뒤집기, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240606/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240606/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,15 +1,23 @@
 N = int(input())
 W, B = 0, 0
 
+last = ""
 for _ in range(N):
     a,b = input().split()
     a = int(a)
     if b == "L":
+        
         W += a
+        if last == "L":
+            W -= 1
+        
         B = B - a if B > a else 0
-
+        last = "L"
     else: 
         B += a
+        if last == "B":
+            B -= 1
         W = W - a if W > a else 0
-    
+        
+        last = "B"
 print(W, B)
