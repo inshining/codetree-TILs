0a20ffe1976575751c61f4a6918869f54dab69e7
inshining <86528640+inshining@users.noreply.github.com>
2024-06-16 15:52:31 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240616/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
+++ 240616/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.py
@@ -3,21 +3,21 @@ board = [list(map(int, input().split())) for _ in range(n)]
 
 dp = [[[-int(1e9), int(1e9)]] * n for _ in range(n)]
 
-dp[0][0] = [board[0][0], board[0][0]]
+dp[n-1][n-1] = [board[n-1][n-1], board[n-1][n-1]]
 
-for x in range(1, n):
-    v = board[0][x]
-    dp[0][x] = [max(dp[0][x-1][0], v), min(dp[0][x-1][1], v)]
+for x in range(n-2, -1, -1):
+    v = board[n-1][x]
+    dp[n-1][x] = [max(dp[n-1][x+1][0], v), min(dp[n-1][x+1][1], v)]
 
-for y in range(1, n):
-    v = board[y][0]
-    dp[y][0] = [max(dp[y-1][0][0], v), min(dp[y-1][0][1], v)]
+for y in range(n-2, -1,-1):
+    v = board[y][n-1]
+    dp[y][n-1] = [max(dp[y+1][n-1][0], v), min(dp[y+1][n-1][1], v)]
 
-for y in range(1, n):
-    for x in range(1, n):
+for y in range(n-2, -1, -1):
+    for x in range(n-2, -1, -1):
         v = board[y][x]
-        a1, a2= dp[y-1][x]
-        b1, b2 = dp[y][x-1]
+        a1, a2= dp[y+1][x]
+        b1, b2 = dp[y][x+1]
 
         a1, a2 = max(a1, v), min(a2, v)
         b1, b2 = max(b1, v), min(b2, v)
@@ -34,4 +34,4 @@ for y in range(1, n):
 
 # for row in dp:
 #     print(row)
-print(dp[n-1][n-1][0] - dp[n-1][n-1][1])
+print(dp[0][0][0] - dp[0][0][1])
