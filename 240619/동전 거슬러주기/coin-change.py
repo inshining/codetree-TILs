9d04b8def1f367ee28eac6774617a8ca42903b3c
inshining <86528640+inshining@users.noreply.github.com>
2024-06-19 15:04:07 +0900

    Title: 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240619/동전 거슬러주기/coin-change.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+coins = list(map(int, input().split()))
+
+dp = [int(1e9)] * (M + 1)
+
+for cc in coins:
+    dp[cc] = 1
+
+for i in range(1, M+1):
+    for j in range(N):
+        if i >= coins[j]:
+            if dp[i - coins[j]] == int(1e9):
+                continue
+            
+            dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+
+ans = dp[M]
+if ans == int(1e9):
+    ans = -1
+print(ans)
