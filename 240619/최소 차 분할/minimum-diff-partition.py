7c4eb9043f65c3c00ebc480da8d31886e2b6b90e
inshining <86528640+inshining@users.noreply.github.com>
2024-06-19 17:46:57 +0900

    Title: 최소 차 분할, Time: 344ms, Memory: 54MB, Status: Passed - CodeTree

--- 240619/최소 차 분할/minimum-diff-partition.py
+++ 240619/최소 차 분할/minimum-diff-partition.py
@@ -1,30 +1,20 @@
 n = int(input())
-l = list(map(int, input().split()))
-dp = [-1] * (sum(l) + 1)
-dp[0] = 0
-
-for i in l:
-    for j in range(1, sum(l) + 1):
-        if j >= i:
-            if dp[j-i] == -1:
-                continue
-
-            dp[j] = max(dp[j], dp[j-i] + i)
-
+l = [0] + list(map(int, input().split()))
 total = sum(l)
 
-vs = [0]
-ans = total
-for i in l:
-    temp = []
-    for v in vs:
-        temp.append(v + i)
-    vs.extend(temp)
+dp = [ [False] * (total + 1) for _ in range(n+1)]
+dp[0][0] = True
 
-ans = int(1e9)
-for i in range(1, len(vs) - 1):
-    ans = min(ans, abs(total - (vs[i] * 2)))
-# for i in range(1, total):
-    # if dp[i] > 0:
-    #     ans = min(ans, abs(total - (dp[i] * 2)))
+ans = total 
+
+for i in range(1, n+1):
+    for j in range(total+1):
+        if j >= l[i] and dp[i-1][j-l[i]]:
+            dp[i][j] = True
+        if dp[i-1][j]:
+            dp[i][j] = True 
+
+for i in range(1, total):
+    if dp[n][i]:
+        ans = min(ans, abs(i - (total - i)))
 print(ans)
