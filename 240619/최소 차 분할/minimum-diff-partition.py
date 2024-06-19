0310fdda1984e9a5b1a5588780353507e0a13308
inshining <86528640+inshining@users.noreply.github.com>
2024-06-19 17:21:18 +0900

    Title: 최소 차 분할, Time: 1018ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240619/최소 차 분할/minimum-diff-partition.py
+++ 240619/최소 차 분할/minimum-diff-partition.py
@@ -13,9 +13,18 @@ for i in l:
 
 total = sum(l)
 
+vs = [0]
 ans = total
-for i in range(1, total):
-    if dp[i] > 0:
-        ans = min(ans, abs(total - (dp[i] * 2)))
+for i in l:
+    temp = []
+    for v in vs:
+        temp.append(v + i)
+    vs.extend(temp)
 
+ans = int(1e9)
+for i in range(1, len(vs) - 1):
+    ans = min(ans, abs(total - (vs[i] * 2)))
+# for i in range(1, total):
+    # if dp[i] > 0:
+    #     ans = min(ans, abs(total - (dp[i] * 2)))
 print(ans)
