ebe566ce55b1613aeb205ef2c0f517bcf698f20b
inshining <86528640+inshining@users.noreply.github.com>
2024-06-20 14:59:46 +0900

    Title: 숫자 전쟁, Time: 147ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240620/숫자 전쟁/number-war.py
@@ -0,0 +1,25 @@
+n = int(input())
+a = [0] + list(map(int, input().split()))
+b = [0] + list(map(int, input().split()))
+
+dp = [[-1] * (n+1) for _ in range(n+1)]
+
+dp[0][0] = 0
+
+for i in range(n):
+    for j in range(n):
+        if dp[i][j] == -1:
+            continue
+        
+        if a[i+1] < b[j+1]:
+            dp[i+1][j] = max(dp[i][j], dp[i+1][j])
+        
+        if a[i+1] > b[j+1]:
+            dp[i][j+1] = max(dp[i][j] + b[j+1], dp[i][j+1])
+        dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j])
+
+ans = 0
+for i in range(1, n+1):
+    ans = max(ans, dp[i][n])
+    ans = max(ans, dp[n][i])
+print(ans)
