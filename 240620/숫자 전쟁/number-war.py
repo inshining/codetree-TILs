8a23a2dc1c727eaa9a4f9a7ea5cea0bf74477dd4
inshining <86528640+inshining@users.noreply.github.com>
2024-06-20 15:00:34 +0900

    Title: 숫자 전쟁, Time: 294ms, Memory: 29MB, Status: Passed - CodeTree

--- 240620/숫자 전쟁/number-war.py
+++ 240620/숫자 전쟁/number-war.py
@@ -19,7 +19,7 @@ for i in range(n):
         dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j])
 
 ans = 0
-for i in range(1, n+1):
+for i in range(n+1):
     ans = max(ans, dp[i][n])
     ans = max(ans, dp[n][i])
 print(ans)
