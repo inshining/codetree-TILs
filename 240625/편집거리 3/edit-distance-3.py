fcda6a7a90dcf49aa46a34b27d0ebb69588c5fcb
inshining <86528640+inshining@users.noreply.github.com>
2024-06-25 09:40:18 +0900

    Title: 편집거리 3, Time: 164ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240625/편집거리 3/edit-distance-3.py
@@ -0,0 +1,19 @@
+A = "#" + input()
+B = "#" + input()
+
+dp = [[0] * len(B) for _ in range(len(A)) ]
+
+for i in range(1, len(B)):
+    dp[0][i] = i
+
+for i in range(1, len(A)):
+    dp[i][0] = i
+
+for i in range(1, len(A)):
+    for j in range(1, len(B)):
+        if A[i] == B[j]:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + 1
+
+print(dp[len(A)-1][len(B)-1])
