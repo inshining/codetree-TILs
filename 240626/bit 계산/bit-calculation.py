1a18498fca20d2d217502dcabe16395e540fe9c1
inshining <86528640+inshining@users.noreply.github.com>
2024-06-26 18:08:55 +0900

    Title: bit 계산, Time: 476ms, Memory: 28MB, Status: Passed - CodeTree

--- 240626/bit 계산/bit-calculation.py
+++ 240626/bit 계산/bit-calculation.py
@@ -2,16 +2,16 @@ N = int(input())
 
 y = 0
 for _ in range(N):
-    
     command, *v = input().split()
-    x =0
+    x = 0
     if len(v):
-
         x = int(v[0])
     if command == "add":
-        y = y + (1 << x)
+        if ((y >> x) & 1) == 0:
+            y = y + (1 << x)
     elif command == "delete":
-        y = y - (1 << x)
+        if ((y >> x) & 1) == 1:
+            y = y - (1 << x)
     elif command == "print":
         print(y >> x & 1)
     elif command == "toggle":
