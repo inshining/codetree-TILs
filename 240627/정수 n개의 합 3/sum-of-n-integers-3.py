37e5746370d789c336ffbb79e7cae51a2d4e89bd
inshining <86528640+inshining@users.noreply.github.com>
2024-06-27 21:14:17 +0900

    Title: 정수 n개의 합 3, Time: 144ms, Memory: 27MB, Status: Passed - CodeTree

---
+++ 240627/정수 n개의 합 3/sum-of-n-integers-3.py
@@ -0,0 +1,15 @@
+N, K= map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * (N+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + board[i-1][j-1]
+
+ans = 0
+for i in range(K, N+1):
+    for j in range(K, N+1):
+        v = dp[i][j] - dp[i-K][j] - dp[i][j-K] + dp[i-K][j-K]
+        ans = max(ans, v)
+print(ans)
