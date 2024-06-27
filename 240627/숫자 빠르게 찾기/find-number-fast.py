2de8b767eb506e65add835bd99e6ca64f766c605
inshining <86528640+inshining@users.noreply.github.com>
2024-06-27 13:17:10 +0900

    Title: 숫자 빠르게 찾기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240627/숫자 빠르게 찾기/find-number-fast.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+targets = [int(input()) for _ in range(M)]
+
+def find(t):
+    idx = -1
+    left, right = 0, len(A)
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] == t:
+            idx = mid
+            break
+        if A[mid] > t:
+            right = mid -1
+        else:
+            left = mid + 1
+    return idx + 1 if idx != -1 else -1
+
+for t in targets:
+    print(find(t))
