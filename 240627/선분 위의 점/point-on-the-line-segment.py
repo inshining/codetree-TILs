5e21dac7690c2e4c534c69d1b03689b8344cfb5d
inshining <86528640+inshining@users.noreply.github.com>
2024-06-27 14:56:33 +0900

    Title: 선분 위의 점, Time: 538ms, Memory: 38MB, Status: Passed - CodeTree

--- 240627/선분 위의 점/point-on-the-line-segment.py
+++ 240627/선분 위의 점/point-on-the-line-segment.py
@@ -2,27 +2,25 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 targets = [list(map(int, input().split())) for _ in range(M)]
 A.sort()
-
 def find(t):
     left = 0
     right = len(A) - 1
-    max_idx = -1
+    min_idx = N
 
     while left <= right:
         mid = (left + right) // 2
-        if A[mid] <= t:
-            max_idx = max(max_idx, mid)
-            left = mid +1
+        if A[mid] >= t:
+            min_idx = min(min_idx, mid)
+            right = mid -1
 
         else:
-            right = mid - 1
-    return max_idx
+            left = mid + 1
+    return min_idx
 
 for a,b in targets:
     end = find(b)
     start = find(a)
-    print(end, start)
-    # ans = end - start
-    # if end < N and A[end] == b:
-    #     ans += 1
-    # print(ans)
+    ans = end - start
+    if end < N and A[end] == b:
+        ans += 1
+    print(ans)
