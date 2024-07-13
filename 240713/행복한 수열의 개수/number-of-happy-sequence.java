5a52cddb238643fb80ebe5db0ea3b6e654ca5b5f
inshining <86528640+inshining@users.noreply.github.com>
2024-07-13 17:53:31 +0900

    Title: 행복한 수열의 개수, Time: 288ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 240713/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Main {
+    public static int N, M;
+    public static int[][] board;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        board = new int[N][N];
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j <N; j++){
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < N; i++){
+            int pre = 0;
+            int cnt= 1;
+            for (int j = 0; j<N; j++){
+                if (board[i][j] == pre){
+                    cnt++;
+                } else{
+                    cnt = 1;
+                }
+                pre = board[i][j];
+                if (cnt >= M){
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++){
+            int pre = 0;
+            int cnt= 1;
+            for (int j = 0; j<N; j++){
+                if (board[j][i] == pre){
+                    cnt++;
+                } else{
+                    cnt = 1;
+
+                }
+                pre = board[i][j];
+                if (cnt >= M){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        System.out.println(ans);
+        
+    }
+}
