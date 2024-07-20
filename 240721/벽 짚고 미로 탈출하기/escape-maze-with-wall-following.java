ad63de006094479705f3f9398d039cd281f45f2c
inshining <86528640+inshining@users.noreply.github.com>
2024-07-21 00:23:49 +0900

    Title: 벽 짚고 미로 탈출하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240721/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class Main {
+    public static int n, r, c, ans;
+    public static char[][] board;
+    public static int[] dy = {-1, 0, 1, 0};
+    public static int[] dx = {0, 1, 0, -1};
+    public static int dir = 1;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        r = sc.nextInt();
+        c = sc.nextInt();
+        r -= 1;
+        c -= 1;
+
+        board = new char[n][n];
+
+        for (int i =0; i < n; i++){
+            String s = sc.next();
+            for (int j = 0; j < n; j++){
+                board[i][j] = s.charAt(j);
+            }
+        }
+
+        go(r,c);
+        System.out.println(ans);
+
+        
+    }
+
+    public static void go(int y, int x){
+        if (ans >= 4 * n){
+            ans = -1;
+            return;
+        }
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+
+        if (out(ny, nx)) {
+            ans++;
+            return;
+        }
+
+        if (board[ny][nx] == '#'){
+            dir -= 1;
+            dir %= 4;
+            go(ny,nx);
+            return;
+        }
+
+        int right_dir = (dir +1 )%4;
+        if (board[ny + dy[right_dir]][nx + dx[right_dir]] == '#'){
+            ans++;
+            go(ny, nx);
+            return;
+        }
+
+        ans += 2;
+        dir = right_dir;
+        go(ny + dy[right_dir], nx + dx[right_dir]);
+        return;
+    }
+
+    public static boolean out(int y, int x){
+        return (y < 0 || n <= y || x < 0 || n <= x);
+    }
+}
