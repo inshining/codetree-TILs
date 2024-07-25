167b41f783587d09d8eb36b1cecb05ae30f4693e
inshining <86528640+inshining@users.noreply.github.com>
2024-07-25 10:30:15 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 320ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 240725/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Pair{
+    int y,x;
+    public Pair(int y, int x){
+        this.y = y;
+        this.x = x;
+    }
+}
+public class Main {
+    public static int n,m;
+    public static int[][] board;
+    public static boolean[][] visit;
+    public static Queue<Pair> q = new LinkedList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        board = new int[n][m];
+        visit = new boolean[n][m];
+
+        for (int i =0 ; i < n; i++){
+            for (int j = 0; j < m; j++){
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] dy = {-1, 0, 1, 0};
+        int[] dx = {0, 1, 0, -1};
+        
+        push(0,0);
+        int ans = 0;
+        while (!q.isEmpty()){
+            Pair p = q.poll();
+            int y = p.y;
+            int x = p.x;
+
+            if (y == n-1 && x == m-1){
+                ans = 1;
+                break;
+            }
+
+            for (int i = 0; i < 4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (ny < 0 || n <= ny || nx < 0 || m <= nx){
+                    continue;
+                }
+
+                if (visit[ny][nx]) continue;
+
+                if (board[ny][nx] == 0) continue;
+
+                push(ny,nx);
+            }
+
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void push(int y, int x){
+        visit[y][x] = true;
+        q.add(new Pair(y,x));
+    }
+
+
+}
