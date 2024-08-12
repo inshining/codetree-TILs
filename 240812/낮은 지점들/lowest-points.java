1c72875770aec7e1af0d56e7a32b81abb11cb927
inshining <86528640+inshining@users.noreply.github.com>
2024-08-12 23:58:44 +0900

    Title: 낮은 지점들, Time: 978ms, Memory: 38MB, Status: Wrong Answer - CodeTree

---
+++ 240812/낮은 지점들/lowest-points.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if (map.containsKey(x)){
+                int v = map.get(x);
+                map.put(x, Math.min(v, y));
+            } else{
+                map.put(x, y);
+            }
+
+        }
+        int ans = 0;
+        for (int v : map.values()){
+            ans += v;
+        }
+        // int ans = map.values().stream().sum();
+        System.out.println(ans);
+    }
+}
