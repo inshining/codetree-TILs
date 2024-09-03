9cba0c09ffb4e5c30bb1e8461fbe576d305ca706
inshining <86528640+inshining@users.noreply.github.com>
2024-09-03 19:15:01 +0900

    Title: 점 개수 세기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240903/점 개수 세기/count-number-of-points.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        TreeSet<Integer> t = new TreeSet<>();
+
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+
+        for (int i = 0; i< n; i++){
+            int k =  sc.nextInt();
+            t.add(k);
+        }
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int idx = 1;
+        for (int k : t){
+            hm.put(k, idx);
+            idx++;
+        }
+
+        for (int i = 0; i < q; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int a2 = Integer.MIN_VALUE;
+            int b2 = Integer.MAX_VALUE;
+
+            if (t.floor(a) != null){
+                a2 = t.floor(a);
+            }
+            if (t.floor(b) != null){
+                b2 = t.floor(b);
+            }
+
+            int f = 0;
+            int c = n;
+            if (a2 != Integer.MIN_VALUE){
+                f = hm.get(a2);
+            }
+
+            if (b2 != Integer.MAX_VALUE){
+                c = hm.get(b2);
+            }
+
+            int ans = c - f;
+            if (a == a2){
+                ans++;
+            }
+
+            System.out.println(ans);
+        }
+    }
+}
