816a887a1ba14491e5e502bcb35222d1e0eeb426
inshining <86528640+inshining@users.noreply.github.com>
2024-09-25 01:07:43 +0900

    Title: 두 지점 사이의 거리, Time: 541ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240925/두 지점 사이의 거리/distance-between-two-points.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+    public static int ans, total, n;
+    public static ArrayList<Integer> arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> l = new ArrayList<>();
+
+        n = sc.nextInt();
+        for (int i =0; i < n; i++){
+            l.add(sc.nextInt());
+        }
+
+        ans = Integer.MIN_VALUE;
+
+        Collections.sort(l);
+
+        arr = new ArrayList<>();
+        arr.add(0);
+
+        for (int i = 0; i < n; i++){
+            int v = arr.get(arr.size() - 1) + l.get(i);
+            arr.add(v);
+        }
+        total = arr.get(arr.size() - 1);
+        // System.out.println(arr);
+        go(1);
+        System.out.println(ans);
+    }
+
+    public static void go(int dis){
+        if (dis >= n)
+            return;
+
+        for (int i = 0; i < arr.size() - dis; i++){
+            int v = arr.get(i + dis) - arr.get(i); 
+            int min = Math.min(v, total - v);
+            ans = Math.max(ans, min);
+        }
+
+        go(dis+1);
+    }
+}
