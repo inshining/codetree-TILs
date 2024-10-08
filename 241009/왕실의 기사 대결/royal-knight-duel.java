11675415f6ea5f303ce8f2ee19d5c4319b7dcc6b
inshining <86528640+inshining@users.noreply.github.com>
2024-10-09 00:56:54 +0900

    Title: 왕실의 기사 대결, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241009/왕실의 기사 대결/royal-knight-duel.java
@@ -0,0 +1,169 @@
+import java.util.*;
+public class Main {
+	static int L, N, Q;
+	static int[][] board;
+	static int[][] knights;
+	static int[] dy = {-1, 0, 1, 0};
+	static int[] dx = {0, 1, 0, -1};
+	static int[] live;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		L = sc.nextInt();
+		N = sc.nextInt();
+		Q = sc.nextInt();
+		
+		board = new int[L+1][L+1];
+		
+		for(int i =1; i <= L; i++) {
+			for(int j =1; j <= L; j++) {
+				board[i][j] = sc.nextInt();
+			}
+		}
+		
+		knights = new int[N+1][5];
+		live = new int[N+1];
+		
+		for(int i =1; i <= N; i++) {
+			int r = sc.nextInt();
+			int c= sc.nextInt();
+			int h = sc.nextInt();
+			int w = sc.nextInt();
+			int k = sc.nextInt();
+			live[i] = k;
+			knights[i] = new int[] {r,c,h,w,k};
+		}
+		
+		for(int i =0; i < Q; i++) {
+			int id = sc.nextInt();
+			int d = sc.nextInt();
+			List<Integer> list = bfs(id, d);
+			
+			if(list.size() == 1 && list.get(0) == -1) continue;
+			
+			knights[id][0] += dy[d];
+			knights[id][1] += dx[d];
+			
+			for(int j = 0; j < list.size(); j++) {
+				int idx = list.get(j);
+				knights[idx][0] += dy[d];
+				knights[idx][1] += dx[d];
+			}
+			
+			delete(list);
+			
+
+		}
+
+		int ans = 0;
+		for(int i =1; i <= N; i++) {
+			if(knights[i][4] > 0) {
+				ans += (live[i] - knights[i][4]);
+			}
+		}
+		System.out.println(ans);
+		
+	}
+	
+	static void delete(List<Integer> list) {
+		for(int i = 0; i < list.size(); i++) {
+			int idx = list.get(i);
+			int[] knight = knights[idx];
+			int r = knight[0];
+			int c = knight[1];
+			int h = knight[2];
+			int w = knight[3];
+			
+			int num = 0;
+			
+			for(int y = r; y <r + h; y++) {
+				for(int x = c; x < c + w; x++) {
+					if (board[y][x] == 1) num++;
+				}
+			}
+			knight[4] -= num;
+		}
+	}
+	
+	static List<Integer> bfs(int id, int d){
+		int[][] map = new int[L+1][L+1];
+		for(int i = 1; i <= N; i++) {
+			int[] knight = knights[i];
+			int r = knight[0];
+			int c = knight[1];
+			int h = knight[2];
+			int w = knight[3];
+			int k = knight[4];
+			if(k <= 0) continue;
+			for(int y = r; y <r + h; y++) {
+				for(int x = c; x < c + w; x++) {
+					map[y][x] = i;
+				}
+			}
+		}
+		
+		List<Integer> result = new ArrayList<>();
+		
+		Deque<int[]> q = new ArrayDeque<>();
+		List<int[]> l = getCan(id, d);
+		for(int i =0; i < l.size(); i++) {
+			q.offer(l.get(i));
+		}
+		while(!q.isEmpty()) {
+			int[] arr = q.poll();
+			
+			int y = arr[0];
+			int x= arr[1];
+			if(out(y,x) || board[y][x] == 2) {
+				result.clear();
+				result.add(-1);
+				return result;
+			};
+
+			if(map[y][x] > 0) {
+				int idx = map[y][x];
+				result.add(idx);
+				List<int[]> nextL = getCan(idx, d);
+				for(int i =0; i < nextL.size(); i++) {
+					q.offer(nextL.get(i));
+				}
+			}
+		}
+		return result;
+	}
+		
+	static boolean out(int y, int x) {
+		return y < 1 || L < y || x < 1 || L < x;
+	}
+	
+	
+	static List<int[]> getCan(int id, int d){
+		int[] knight = knights[id];
+		int r = knight[0];
+		int c = knight[1];
+		int h = knight[2];
+		int w = knight[3];
+		int k = knight[4];
+		
+		List<int[]> result = new ArrayList<>();
+		
+		if(d == 0) {
+			for(int i = 0; i < w; i++) {
+				result.add(new int[] {r-1, c+i});
+			}
+		}else if(d==1) {
+			for(int i =0; i < h; i++) {
+				result.add(new int[] {r+i, c+w});
+			}
+		} else if(d==2) {
+			for(int i =0; i < w; i++) {
+				result.add(new int[] {r+h, c+i});
+			}
+		} else if(d==3) {
+			for(int i =0; i < h; i++) {
+				result.add(new int[] {r+i, c-1});
+			}
+		}
+		return result;
+	}
+}
