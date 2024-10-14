25e7ce8d5642f213c1c121d67d74c475cc23da5f
inshining <86528640+inshining@users.noreply.github.com>
2024-10-14 17:49:25 +0900

    Title: 동일하게 분할, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241014/동일하게 분할/equal-partition.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] coins = new int[N];
+        int total = 0;
+        for(int i = 0; i < N; i++){
+            coins[i] = sc.nextInt();
+            total += coins[i];
+        }
+
+        if(total % 2 == 1){
+            System.out.println("No");
+        }else{
+            int target = total / 2;
+
+            int[] dp = new int[target+1];
+            int minV = Integer.MIN_VALUE;
+
+            Arrays.fill(dp, minV);
+            dp[0] = 0;
+            for(int i = 0; i < N; i++){
+                for(int j = target; j >= 0; j--){
+                    if(j >= coins[i]){
+                        if (dp[j - coins[i]] == minV) continue;
+                        dp[j] = Math.max(dp[j], dp[j-coins[i]] + 1);
+                    }
+                }
+                System.out.println(Arrays.toString(dp));
+            }
+            String ans = dp[target] > 0 ? "Yes" : "No";
+            System.out.println(ans);
+        }
+    }
+}
