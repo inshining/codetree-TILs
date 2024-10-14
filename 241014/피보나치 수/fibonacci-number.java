19efea52ec153602007ab4a980795a15d6527338
inshining <86528640+inshining@users.noreply.github.com>
2024-10-14 14:29:49 +0900

    Title: 피보나치 수, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 241014/피보나치 수/fibonacci-number.java
+++ 241014/피보나치 수/fibonacci-number.java
@@ -4,7 +4,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] dp = new int[N+1];
+        int[] dp = new int[46];
         dp[1] = 1;
         dp[2] = 1;
 
