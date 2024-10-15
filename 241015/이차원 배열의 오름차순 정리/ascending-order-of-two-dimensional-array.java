27755761a1a9f71ddc71831a9c374c3acf95a565
inshining <86528640+inshining@users.noreply.github.com>
2024-10-15 13:31:58 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 162ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241015/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextLong();
+        long K = sc.nextLong();
+
+        long left = 1;
+        long right = N * N;
+
+        long ans = Long.MAX_VALUE;
+
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long order = getNum(mid, N);
+            // System.out.println(left + " " + mid + " " + right + " " + order);
+
+            if(order >= K){
+                ans = Math.min(ans, mid);
+                right = mid - 1;
+            }else left = mid + 1;
+        }
+        System.out.println(ans);
+    }
+    public static long getNum(long mid, long N){
+        int num = 0;
+        for(long i = 1; i <= N; i++){
+            num += Math.min(N, (mid / i));
+        }
+        return num;
+    }
+}
