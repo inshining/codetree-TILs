aac2abc6d0d0254f2b94c021ef313a3ab24e5cd6
inshining <86528640+inshining@users.noreply.github.com>
2024-10-15 14:27:05 +0900

    Title: 최대 거리의 점, Time: 692ms, Memory: 23MB, Status: Passed - CodeTree

--- 241015/최대 거리의 점/maximum-distance-point.java
+++ 241015/최대 거리의 점/maximum-distance-point.java
@@ -1,15 +1,17 @@
 import java.util.*;
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         int[] arr = new int[N];
-
+        
         for(int i= 0; i < N; i++){
-            arr[i] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
