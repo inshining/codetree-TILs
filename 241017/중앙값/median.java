4aaf881f54a5c1e1dab9acf4fc60af8212a025d8
inshining <86528640+inshining@users.noreply.github.com>
2024-10-17 19:55:19 +0900

    Title: 중앙값, Time: 1129ms, Memory: 35MB, Status: Wrong Answer - CodeTree

---
+++ 241017/중앙값/median.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+    public static PriorityQueue<Integer> right = new PriorityQueue<>();
+    public static int mid;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        for (int i = 0; i < t; i++){
+            int n = sc.nextInt();
+            int mid = -1;
+            for (int j = 1; j <= n; j++){
+                int k = sc.nextInt();
+                if(mid == -1){
+                    mid = k;
+                } else{
+                    if(mid < k){
+                        right.add(k);
+                    } else{
+                        left.add(k);
+                    }
+                }
+                
+                if (j % 2 == 1){
+                    if(right.size() != left.size()) {
+                        if(right.size() < left.size()){
+                            right.add(mid);
+                            mid = left.poll();
+                        }else{
+                            left.add(mid);
+                            mid = right.poll();
+                        }
+                    }
+                    System.out.print(mid + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void print(int mid){
+        System.out.println(mid);
+    }
+}
