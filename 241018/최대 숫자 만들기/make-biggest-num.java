4e08638230f558236c9307d6c075d105382ab9f0
inshining <86528640+inshining@users.noreply.github.com>
2024-10-18 20:37:10 +0900

    Title: 최대 숫자 만들기, Time: 958ms, Memory: 54MB, Status: Passed - CodeTree

--- 241018/최대 숫자 만들기/make-biggest-num.java
+++ 241018/최대 숫자 만들기/make-biggest-num.java
@@ -4,36 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Long[] arr = new Long[n];
+        String[] arr = new String[n];
         for(int i = 0; i < n; i++){
-            arr[i] = sc.nextLong();
+            arr[i] = sc.next();
         }
         
-        Arrays.sort(arr, new Comparator<Long>(){
+        Arrays.sort(arr, new Comparator<String>(){
             @Override
-            public int compare(Long a, Long b){
-                int aSize = (int)(Math.log10(a) + 1);
-                int bSize = (int)(Math.log10(b) + 1);
-                long A = (long) (Math.pow(10, aSize) * b );
-                A += a;
-                long B = (long) ( Math.pow(10, bSize) * a );
-                B += b;
-                if( A > B){
-                    return 1;
-                } else if(A < B){
-                    return -1;
+            public int compare(String a, String b){
+                String A = a + b;
+                String B = b + a;
+                for(int i = 0; i < A.length(); i++){
+                    if(A.charAt(i) > B.charAt(i)) return -1;
+                    else if(A.charAt(i) < B.charAt(i)) return 1;
                 }
                 return 0;
             }
         });
-        long ans = arr[0];
-        for(int i = 1; i < n; i++){
-            int size = (int)(Math.log10(arr[i]) + 1);
-            ans *= (long) (Math.pow(10, size));
-            ans += arr[i];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append(arr[i]);
         }
         // System.out.println(Arrays.toString(arr));
-        System.out.println(ans);
+        System.out.println(sb.toString());
 
         // for(int = )
     }
