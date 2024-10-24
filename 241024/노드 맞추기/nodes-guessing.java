b3c319c07990d111f51b7e2f32bce649345110a5
inshining <86528640+inshining@users.noreply.github.com>
2024-10-24 14:37:09 +0900

    Title: 노드 맞추기, Time: 160ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 241024/노드 맞추기/nodes-guessing.java
+++ 241024/노드 맞추기/nodes-guessing.java
@@ -7,12 +7,12 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        TreeMap<Character, ArrayList<Character>> board = new TreeMap<Character, ArrayList<Character>>();
-        TreeMap<Character, ArrayList<Character>> ans = new TreeMap<Character, ArrayList<Character>>();
+        TreeMap<String, ArrayList<String>> board = new TreeMap<String, ArrayList<String>>();
+        TreeMap<String, ArrayList<String>> ans = new TreeMap<String, ArrayList<String>>();
 
-        TreeMap<Character, Integer> map = new TreeMap<>();
+        TreeMap<String, Integer> map = new TreeMap<>();
         for(int i = 0; i < N; i++){
-            char c = st.nextToken().charAt(0);
+            String c = st.nextToken();
             board.put(c, new ArrayList<>());
             map.put(c, 0);
             ans.put(c, new ArrayList<>());
@@ -21,18 +21,18 @@ public class Main {
 
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
-            char x = st.nextToken().charAt(0);
-            char y = st.nextToken().charAt(0);
+            String x = st.nextToken();
+            String y = st.nextToken();
             board.get(y).add(x);
             int v = map.get(x);
             map.put(x, v+1);
         }
 
-        PriorityQueue<Character> pq = new PriorityQueue<>();
+        PriorityQueue<String> pq = new PriorityQueue<>();
 
         StringBuilder sb = new StringBuilder();
         int num = 0;
-        for(char c : map.keySet()){
+        for(String c : map.keySet()){
             if(map.get(c) == 0){
                 pq.offer(c);
                 sb.append(c);
@@ -43,9 +43,9 @@ public class Main {
         sb.append('\n');
 
         while(!pq.isEmpty()){
-            char c = pq.poll();
+            String c = pq.poll();
             
-            for(char next : board.get(c)){
+            for(String next : board.get(c)){
                 int v = map.get(next);
                 map.put(next, v-1);
                 if(map.get(next) == 0){
@@ -55,13 +55,13 @@ public class Main {
             }
         }
 
-        for(char c : ans.keySet()){
+        for(String c : ans.keySet()){
             sb.append(c);
             sb.append(' ');
             sb.append(ans.get(c).size());
             sb.append(' ');
             for(int i = 0; i < ans.get(c).size(); i++){
-                char child = ans.get(c).get(i);
+                String child = ans.get(c).get(i);
                 sb.append(child);
                 sb.append(' ');
             }
