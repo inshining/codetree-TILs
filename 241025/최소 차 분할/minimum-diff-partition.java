1c47a4c8dd3f0b7ffe84c3f2460e9489ff436823
inshining <86528640+inshining@users.noreply.github.com>
2024-10-25 23:14:38 +0900

    Title: 최소 차 분할, Time: 220ms, Memory: 26MB, Status: Passed - CodeTree

--- 241025/최소 차 분할/minimum-diff-partition.java
+++ 241025/최소 차 분할/minimum-diff-partition.java
@@ -1,53 +1,33 @@
 import java.util.*;
 public class Main {
+    static int MAX_N = 101;
+    static int MAX_M = 100001;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[] arr = new int[N];
-        for(int i = 0; i < N; i++){
+        int[] arr = new int[MAX_N];
+        for(int i = 1; i <= N; i++){
             arr[i] = sc.nextInt();
         }
 
-        int[][][] dp = new int[N][N+1][N+1];
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j <= N; j++){
-                for(int k = 0; k <= N; k++){
-                    dp[i][j][k] = Integer.MAX_VALUE;
-                }
-            }
+        int total = 0;
+        for(int i = 1; i <= N; i++){
+            total += arr[i];
         }
-        dp[0][1][0] = arr[0];
-        dp[0][0][1] = arr[0];
 
-        for(int i = 1; i < N; i++){
-            int total = i + 1;
-            for(int j = 0; j <= total; j++){
-                int k = total - j;
-                if(j > 0){
-                    int a = Math.abs(dp[i][j][k]);
-                    int b= Math.abs(dp[i-1][j-1][k] + arr[i]);
-                    if(a > b){
-                        dp[i][j][k] = dp[i-1][j-1][k] + arr[i];
-                    }
-                }
-
-                if(k > 0){
-                    int a = Math.abs(dp[i][j][k]);
-                    int b= Math.abs(dp[i-1][j][k-1] - arr[i]);
-                    if(a > b){
-                        dp[i][j][k] = dp[i-1][j][k-1] - arr[i];
-                    }
-                }
+        boolean[][] dp = new boolean[MAX_N][MAX_M];
+        dp[0][0] = true;
+        for(int i = 1; i<= N; i++){
+            int v = arr[i];
+            for(int j = 0; j < MAX_M; j++){
+                if(v <= j && dp[i-1][j-v]) dp[i][j] = true;
+                if(dp[i-1][j]) dp[i][j] = true;
             }
         }
 
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0; i <= N; i++){
-            for(int j = 0; j <= N; j++){
-                if(ans > Math.abs(dp[N-1][i][j])){
-                    ans = Math.abs(dp[N-1][i][j]);
-                }
-            }
+        int ans = total;
+        for(int j = 0; j <= total; j++){
+            if(dp[N][j]) ans = Math.min(ans, Math.abs(j - (total - j)));
         }
         System.out.println(ans);
     }
