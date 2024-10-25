5e1af7456bd621affddc44c11b98bd370f402b03
inshining <86528640+inshining@users.noreply.github.com>
2024-10-26 00:46:34 +0900

    Title: 경험치를 빠르게 얻기, Time: 171ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 241026/경험치를 빠르게 얻기/gain-exp-quickly.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main {
+    static int MAX_M = 10001;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] exe = new int[N+1];
+        int[] times = new int[N+1];
+        for(int i = 1; i <= N; i++){
+            exe[i] = sc.nextInt();
+            times[i] = sc.nextInt();
+        }
+
+        int[][] dp = new int[N+1][MAX_M];
+        for(int i = 1; i <= N; i++){
+            int e = exe[i];
+            int t = times[i];
+            for(int j = 0; j < MAX_M; j++){
+                if(t <= j){
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-t] + e);
+                }
+                dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < MAX_M; i++){
+            if(dp[N][i] >= M){
+                ans = i;
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
