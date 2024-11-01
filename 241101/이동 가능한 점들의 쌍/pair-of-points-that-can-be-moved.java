cd8eb2bf55d3597567bad5d8836d070b7ec2f58f
inshining <86528640+inshining@users.noreply.github.com>
2024-11-01 16:00:36 +0900

    Title: 이동 가능한 점들의 쌍, Time: 335ms, Memory: 16MB, Status: Passed - CodeTree

--- 241101/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
+++ 241101/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.java
@@ -2,64 +2,57 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     static int INF = (int)1e9;
-    public static void main(String[] args) throws IOException {
-        // 여기에 코드를 작성해주세요.
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-        int q = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
 
-        int[][] graph = new int[n+1][n+1];
-        //init
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= n; j++){
-                graph[i][j] = INF;
-            }
-            graph[i][i] = 0;
+        int[][] board = new int[N+1][N+1];
+        for(int i = 0; i <= N; i++){
+            Arrays.fill(board[i], INF);
+            board[i][i] = 0;
         }
 
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            graph[s][e] = v;
+            int w = Integer.parseInt(st.nextToken());
+            board[s][e] = w;
         }
 
-        //floydWarshall
 
-        for(int k = 1; k <= n; k++){
-            for(int i = 1; i <= n; i++){
-                for(int j = 1; j <= n; j++){
-                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+        for(int p = 1; p <= N; p++){
+            for(int i = 1; i <= N; i++){
+                for(int j = 1; j <= N; j++){
+                    board[i][j] = Math.min(board[i][j], board[i][p] + board[p][j]);
                 }
             }
         }
 
-        int isGo = 0;
-        long answer = 0;
-        for(int testCase = 0; testCase < q; testCase++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
 
-            int dist = (int)1e9;
-            for(int i = 1; i <= p; i++){
-                dist = Math.min(dist, graph[start][i] + graph[i][end]);
+        int cnt = 0;
+        long ans = 0;
+        for(int i = 0; i < Q; i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int dist = INF;
+            for(int j = 1; j <= P; j++){
+                dist = Math.min(dist, board[s][j] + board[j][e]);
             }
 
             if(dist >= INF) continue;
-
-            answer += dist;
-            isGo++;
-
+            ans+= dist;
+            cnt++;
         }
-        System.out.println(isGo);
-        System.out.println(answer);
+        // System.out.println(isRed[1][2]);
+
+        System.out.println(cnt);
+        System.out.println(ans);
     }
 }
