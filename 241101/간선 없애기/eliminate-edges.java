8a4037fc862d7df610c66db20bb0aa5e2ee3b7f9
inshining <86528640+inshining@users.noreply.github.com>
2024-11-01 14:26:13 +0900

    Title: 간선 없애기, Time: 552ms, Memory: 19MB, Status: Wrong Answer - CodeTree

--- 241101/간선 없애기/eliminate-edges.java
+++ 241101/간선 없애기/eliminate-edges.java
@@ -13,44 +13,23 @@ public class Main {
             Arrays.fill(board[i], Integer.MAX_VALUE);
         }
 
+        int[][] edges = new int[M][3];
+
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
             board[s][e] = w;
+            edges[i] = new int[]{s,e,w};
         }
 
-        int[] history = new int[N+1];
-        int[] dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[0] - o2[0]);
-        dist[1] = 0;
-        pq.offer(new int[]{0, 1});
-        while(!pq.isEmpty()){
-            int[] l = pq.poll();
-            int w = l[0];
-            int edge = l[1];
-
-            if(dist[edge] < w) continue;
-
-            for(int i = 1; i <= N; i++){
-                if(board[edge][i] == Integer.MAX_VALUE) continue;
-                int nextV = board[edge][i];
-                if(dist[edge] + nextV < dist[i]){
-                    dist[i] = dist[edge] + nextV;
-                    history[i] = edge;
-                    pq.offer(new int[]{dist[i], i});
-                }
-            }
-        }
-        int minDist = dist[N];
-        List<Integer> route = find(N, history);
+        int minDist = go(board, N);
+        
         Set<Integer> can = new HashSet<>();
-        for(int i = 0; i < route.size() - 1; i++){
-            int s = route.get(i);
-            int e = route.get(i+1);
+        for(int i = 0; i < M; i++){
+            int s = edges[i][0];
+            int e = edges[i][1];
             int temp = board[s][e];
             board[s][e] = Integer.MAX_VALUE;
             int minD = go(board, N);
